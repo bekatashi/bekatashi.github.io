a58e01e61f3d799955a55845c0accc763b552456
bekachan <bekatasi7@gmail.com>
2022-05-20 21:15:52 +0600

    added swagger and orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Order
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from orders import serializers
 
-# Create your views here.
+
+class OrdersViewSet(ModelViewSet):
+    class Meta:
+        fields = '__all__'
+        model = Order
+    queryset = Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
